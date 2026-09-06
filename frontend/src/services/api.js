a2df7689b7acdf68da0b421e3255690da05de5b6
Bhanu Teja <bhanutejadasari21@gmail.com>
2026-09-06 17:31:48 +0530

    configure production api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,7 @@ import {
 } from "./loadingController";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:  import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 api.interceptors.request.use((config) => {
