00304c7cd0536787632456d25a03aad3662f18bb
Bhanu Teja <bhanutejadasari21@gmail.com>
2026-09-06 17:07:41 +0530

    prepare backend for deployment

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,13 @@ app.use(
 
 app.use(express.json());
 
+app.get("/api/health", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "API is running"
+    });
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/question-papers", questionPaperRoutes);
